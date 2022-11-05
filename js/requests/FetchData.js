ab72929747b811ae2a59005542a59592e17762da
Armand WADJI <armand_wadji@yahoo.fr>
2022-11-05 18:10:46 +0100

    error 404 implementation

--- js/requests/FetchData.js
+++ js/requests/FetchData.js
@@ -1,9 +1,12 @@
+import error404 from "../display/ERROR404.js";
+
 const fetchData = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (error) {
+    document.body.innerHTML = error404();
     throw new error("your request is fail, check the url.");
   }
 };
